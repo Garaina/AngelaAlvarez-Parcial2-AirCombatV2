c991369a63317dbee73a8a7cd33272f64a302839
Angela Maria Alvarez <angelaalhe@unisabana.edu.co>
2018-04-18 20:36:29 -0500

    Parcial Angelita V1

---
+++ AngelaAlvarez-AirCombatV2/Assets/Scripts/Avion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate (0, 0, 10f * Time.deltaTime);
+
+	
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate (0.0f, 10f * Time.deltaTime, 0.0f);
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate (0.0f, -10f * Time.deltaTime, 0.0f);
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Translate (10f * Time.deltaTime, 0.0f, 0.0f);
+			}
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Translate (-10f * Time.deltaTime, 0.0f, 0.0f);
+			}
+
+
+			if (Input.GetKeyDown (KeyCode.Space)) {	
+
+				GameObject missile = GameObject.Instantiate (
+					Resources.Load ("Prefab/Missil")as GameObject);
+				missile.transform.SetParent (this.gameObject.transform);
+				missile.transform.localPosition = new Vector3 (3.16f, 1.03f, -0.71f);
+				missile.transform.SetParent (null);
+
+			GameObject missile2 = GameObject.Instantiate (
+				Resources.Load ("Prefab/Missil")as GameObject);
+			missile2.transform.SetParent (this.gameObject.transform);
+			missile2.transform.localPosition = new Vector3 (-3.16f, 1.03f, -0.71f);
+			missile2.transform.SetParent (null);
+
+		}
+	}
+}
---
+++ AngelaAlvarez-AirCombatV2/Assets/Scripts/Missil.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// mueve el avion hacia el frente
+		this.transform.Translate(0.0f,0.0f, 155.0f * Time.deltaTime);
+
+	}
+	void OnCollisionEnter(Collision collision){
+
+		if (collision.collider.gameObject.tag == "MarsAlien") {
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefab/MarsExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+			//Cuando el misil colisiona con la plataforma la elimina
+			Destroy (collision.collider.gameObject);
+			Destroy (this.gameObject);
+		}
+		if (collision.collider.gameObject.tag == "JupiterAlien") {
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefab/JupiterExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+			//Cuando el misil colisiona con la plataforma la elimina
+			Destroy (collision.collider.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+}
